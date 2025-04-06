a2845757d409cf716cd631d1d2ea43ca2204fb72
HalaszIza <halasziza@gmail.com>
2025-04-06 17:43:57 +0200

    az egyik alkalmazas nagyjabol kesz, meg a gombokat kell megcsinalni. plusz en is belaraktam a footerbe a nevem + neptun kodom, majd te is ird bele. koszonom :)

---
+++ react/src-1/src/Game/data.js
@@ -0,0 +1,76 @@
+const Data = [ 
+	{ 
+		id: 1, 
+		name: "Inception", 
+		img: require('./Images/Inception.png'),
+		matched: false, 
+	}, 
+	{ 
+		id: 2, 
+		name: "Interstellar", 
+		img: require('./Images/Interstellar.jpg'), 
+		matched: false, 
+	}, 
+	{ 
+		id: 3, 
+		name: "Shutter_island", 
+		img: require('./Images/shutter-island.jpg'), 
+		matched: false, 
+	}, 
+	{ 
+		id: 4, 
+		name: "Silence_of_the_lambs", 
+		img: require('./Images/Silence_of_the_lambs.jpg'), 
+		matched: false, 
+	}, 
+	{ 
+		id: 5, 
+		name: "Tenet", 
+		img: require('./Images/Tenet.jpg'), 
+		matched: false, 
+	}, 
+	{ 
+		id: 6, 
+		name: "Zodiac", 
+		img: require('./Images/Zodiac.jpg'), 
+		matched: false, 
+	}, 
+	{ 
+		id: 7, 
+		name: "Inception", 
+		img: require('./Images/Inception.png'), 
+		matched: false, 
+	}, 
+	{ 
+		id: 8, 
+		name: "Interstellar", 
+		img: require('./Images/Interstellar.jpg'), 
+		matched: false, 
+	}, 
+	{ 
+		id: 9, 
+		name: "Shutter_island", 
+		img: require('./Images/shutter-island.jpg'), 
+		matched: false, 
+	}, 
+	{ 
+		id: 10, 
+		name: "Silence_of_the_lambs", 
+		img: require('./Images/Silence_of_the_lambs.jpg'), 
+		matched: false, 
+	}, 
+	{ 
+		id: 11, 
+		name: "Tenet", 
+		img: require('./Images/Tenet.jpg'), 
+		matched: false, 
+	}, 
+	{ 
+		id: 12, 
+		name: "Zodiac", 
+		img: require('./Images/Zodiac.jpg'), 
+		matched: false, 
+	}, 
+]; 
+export default Data;
+
